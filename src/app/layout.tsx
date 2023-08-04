e2acd0d99c82ed3e353108d824ab309bba499fdf
Albert <albertquevedo35@gmail.com>
2023-08-03 22:21:56 -0400

    template front

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css';
+import NavBar from '@/app/layout/NavBar';
+import CssBaseline from '@mui/material/CssBaseline';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang='en'>
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <CssBaseline />
+                <NavBar />
+                {children}
+            </body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 import { useEffect } from 'react';
 import axios from '../libs/axios';
+import ArticlesTable from './articles/components/ArticlesTable';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { ArticleForm } from './articles/components/ArticleForm';
 export default function HomePage() {
     async function fetching() {
         const { data } = await axios.post('/clients', {
@@ -12,7 +17,22 @@ export default function HomePage() {
         });
     }
 
-    return <div>page</div>;
+    return (
+        <Container>
+            <Typography variant='h3' textAlign={'center'} my={5}>
+                Lista de Articulos
+            </Typography>
+            <Box sx={{}}>
+                <ArticlesTable />
+            </Box>
+            <Typography variant='h3' textAlign={'center'} my={5}>
+                Añadir Articulos
+            </Typography>
+            <Box>
+                <ArticleForm />
+            </Box>
+        </Container>
+    );
 }
 
 export async function getServerSideProps() {
